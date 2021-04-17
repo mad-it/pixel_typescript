97c72ef5d7ea908ffa7fdb68aa28883e9edb0d22
Sanket Bhangale <sanketvbhangale@gmail.com>
2021-04-18 01:05:19 +0200

    linting

--- test/finder.test.ts
+++ test/finder.test.ts
@@ -1,48 +1,42 @@
-import * as Algo from "../src/finder"
+import * as Algo from "../src/finder";
 
-test('Test happy cases', () => {
+test("Test happy cases", () => {
+  const arr = [
+    [0, 0, 0, 1],
+    [0, 0, 1, 1],
+    [0, 1, 1, 0],
+  ];
+  expect(Algo.find_pixel_distances(arr, 3, 4)).toEqual([
+    [3, 2, 1, 0],
+    [2, 1, 0, 0],
+    [1, 0, 0, 1],
+  ]);
+});
 
-    const arr = [[0,0,0,1],
-                  [0,0,1,1],
-                  [0,1,1,0]
-                ]
-    expect(Algo.find_pixel_distances(arr, 3, 4)).toEqual([
-      [3, 2, 1, 0],
-      [2,1,0,0],
-      [1,0,0,1]
-    ]
-    );
-  });
+test("Test edge cases", () => {
+  const horizontal = [[0, 0, 0, 1]];
+  expect(Algo.find_pixel_distances(horizontal, 1, 4)).toEqual([[3, 2, 1, 0]]);
+  const vertical = [[0], [0], [0], [1]];
+  expect(Algo.find_pixel_distances(vertical, 4, 1)).toEqual([
+    [3],
+    [2],
+    [1],
+    [0],
+  ]);
+});
 
+test("Test empty inputs", () => {
+  expect(Algo.find_pixel_distances([[]], 0, 0)).toEqual([]);
+  expect(Algo.find_pixel_distances([], 0, 0)).toEqual([]);
+});
 
-  test('Test edge cases', () => {
-
-    const horizontal = [[0,0,0,1]
-                ]
-    expect(Algo.find_pixel_distances(horizontal, 1, 4)).toEqual([
-      [3, 2, 1, 0],
-    ]
-    );
-    const vertical = [[0],[0],[0],[1]]
-    expect(Algo.find_pixel_distances(vertical, 4, 1)).toEqual([
-      [3], [2], [1], [0]],
-    
-    );
-  });
-
-test('Test empty inputs', () => {
-    expect(Algo.find_pixel_distances([[]], 0, 0)).toEqual([
-    ]
-    );
-    expect(Algo.find_pixel_distances([], 0, 0)).toEqual([
-    ]
-    );
-  });
-
-  test('Test unexpected inputs', () => {
-    const arr = [[0,0],
-                  [0,0]
-                ]
-    expect(Algo.find_pixel_distances(arr, 2, 2)).toEqual([[5,6],[6,5]]
-    );
-  });
+test("Test unexpected inputs", () => {
+  const arr = [
+    [0, 0],
+    [0, 0],
+  ];
+  expect(Algo.find_pixel_distances(arr, 2, 2)).toEqual([
+    [5, 6],
+    [6, 5],
+  ]);
+});
